4adee081ed39171ae8e3604574e38893b9bb10db
seachannels <seaschannels@gmail.com>
2024-05-09 11:13:56 +0200

    añadido menu pausa (semi funcional)
    añadido
    - menu pausa
    - pantalla guardando

---
+++ Prueba/Assets/Scripts/MENUS/MenuPausa.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPausa : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public GameObject savingMessage;
+    public bool isPaused;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+        savingMessage.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))   
+        {
+            if (isPaused) 
+            {
+                resumeGame();
+            }
+
+            else 
+            {
+                pauseGame();
+            }
+        }
+    }
+
+    public void pauseGame()
+    {
+        pauseMenu.SetActive(true);
+        isPaused = true;
+        Time.timeScale = 0f;
+
+    }
+
+    public void resumeGame()
+    {
+        pauseMenu.SetActive(false);
+        isPaused = false;
+        Time.timeScale = 1f;
+    }
+
+    public void saveGame() {
+        savingMessage.SetActive(true);
+        Invoke ("resumeGame", 3);
+    }
+
+    public void mainMenu() {
+        Time.timeScale = 1f;
+        SceneManager.LoadSceneAsync(0);
+    }
+
+    public void quit() {
+        Application.Quit();
+    }
+
+}
---
+++ Prueba/Assets/dialogue/dialogo figurinista.ink
@@ -0,0 +1,3 @@
+Necesito que me ayudes con una cosa…
+A tu traje le falta un botón en la espalda que necesito 
+¿Podrías ayudarme a encontrarlo? Estoy muy ocupada.
